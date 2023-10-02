22d7447cc6f320d999f43199e0a9dc9c230b5b74
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-02 18:00:43 +0200

    Add loading and error states to RootContext

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -5,29 +5,54 @@ export const RootContext = createContext({});
 RootContext.displayName = "RootContext";
 
 export const RootContextProvider = ({ children }) => {
-  const [users, setUsers] = useState(null);
-  const [categories, setCategories] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [isLoadingUsers, setIsLoadingUsers] = useState(true);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [errorUsers, setErrorUsers] = useState(null);
+  const [errorCategories, setErrorCategories] = useState(null);
 
   useEffect(() => {
+    // setIsLoadingUsers(true);
     const fetchUsers = async () => {
       const response = await fetch("http://localhost:3003/users");
-      const users = await response.json();
-      setUsers(users);
+      if (response.ok) {
+        const users = await response.json();
+        setUsers(users);
+        setIsLoadingUsers(false);
+      } else {
+        setErrorUsers(`Something went wrong: ${response.statusText}`);
+      }
     };
     fetchUsers();
   }, []);
 
   useEffect(() => {
+    // setIsLoadingCategories(true);
     const fetchCategories = async () => {
       const response = await fetch("http://localhost:3003/categories");
-      const categories = await response.json();
-      setCategories(categories);
+      if (response.ok) {
+        const categories = await response.json();
+        setCategories(categories);
+        setIsLoadingCategories(false);
+      } else {
+        setErrorCategories(`Something went wrong: ${response.statusText}`);
+      }
     };
     fetchCategories();
   }, []);
 
   return (
-    <RootContext.Provider value={{ users, categories }}>
+    <RootContext.Provider
+      value={{
+        users,
+        categories,
+        isLoadingUsers,
+        isLoadingCategories,
+        errorUsers,
+        errorCategories,
+      }}
+    >
       {children}
     </RootContext.Provider>
   );
