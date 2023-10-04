02648eaef67b3df7fa0c09fdecb99fd133e1e369
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-05 00:11:47 +0200

    Add Search, wrap Heading and Search in a Stack container comp

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./Navigation";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Stack } from "@chakra-ui/react";
 import { RootContextProvider } from "../context/RootContext";
+import { Search } from "../components/Search";
 
 export const Root = () => {
   return (
     <RootContextProvider>
-      <Box bg={"gray.100"}>
+      <Box bg={"gray.100"} minH={"100vh"}>
+        <Stack direction={"row"} backgroundColor={"blue.300"}>
+          <Heading size={"md"} textAlign={"center"} p={4}>
+            Event App
+          </Heading>
+          <Search />
+        </Stack>
         <Navigation />
         <Outlet />
       </Box>
