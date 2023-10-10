11ba8a3b6519b8f7df531aa583c8cc3b3d45f153
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-10 22:55:43 +0200

    Add checkbox event handler and helper fn for category selection/filtering

--- src/util/globalFunctions.js
+++ src/util/globalFunctions.js
@@ -7,6 +7,25 @@ export const getData = async (endpoint) => {
   };
 };
 
+// Categories related checkbox handler and support functions
+//
+// function to create checkedItemMap from an Array of objects
+// key: obj.id, init value: false;
+// this map can be used to track the checked state of checkbox group items
+// obj.id and e.target.id has to be the same value and type
+// i.e. event categories in nav menu filter options, and in new event form to set categories
+export const initCheckedItemMap = (objArr) =>
+  objArr.map((item) => [Number(item.id), false]);
+
+// function to handle onChange event in a group of checkboxes
+export const handleCheckboxChanges = (e, checkedMap, setFn) => {
+  const id = e.target.id;
+  const newChecked = new Map([...checkedMap]);
+  const chkd = newChecked.get(Number(id));
+  newChecked.set(Number(id), !chkd);
+  setFn(new Map([...newChecked]));
+};
+
 export const formatDateAndTime = (dateStr) => {
   const date = new Date(dateStr);
   const dateOptions = {
@@ -14,6 +33,11 @@ export const formatDateAndTime = (dateStr) => {
     year: "numeric",
     month: "long",
     day: "numeric",
+  };
+  const shortDateOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
   };
   const timeOptions = {
     hour: "2-digit",
@@ -23,8 +47,9 @@ export const formatDateAndTime = (dateStr) => {
     // timeZoneName: "shortOffset",
   };
 
+  const shortDate = date.toLocaleDateString("en-GB", shortDateOptions);
   const fullDate = date.toLocaleDateString("en-GB", dateOptions);
   const fullTime = date.toLocaleTimeString("en-GB", timeOptions);
 
-  return { date: fullDate, time: fullTime };
+  return { shortDate: shortDate, date: fullDate, time: fullTime };
 };
