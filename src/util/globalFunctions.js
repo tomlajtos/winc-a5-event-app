f41f4fcd0465b82067a560c2a4597ba95b077797
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-11 22:27:56 +0200

    Add fetchData: replacement for getData, more versatile than getData

--- src/util/globalFunctions.js
+++ src/util/globalFunctions.js
@@ -7,6 +7,28 @@ export const getData = async (endpoint) => {
   };
 };
 
+// helper function for React Router loader function
+export const fetchData = async (
+  endpoints = [{ name: "categories", path: "/categories" }],
+) => {
+  const baseUrl = "http://localhost:3003";
+  // reduce can't handle async
+  const getFn = async (endpoint) =>
+    await fetch(`${baseUrl}${endpoint.path}`).then((r) => r.json());
+
+  const dataObj = endpoints.reduce((obj, endpoint) => {
+    Object.assign(obj, { [endpoint.name]: "" });
+    obj[endpoint.name] = getFn(endpoint);
+    return obj;
+  }, {});
+
+  // re-assign dataObj key values with their awaited self, as they are resolved Promise objects
+  for (let key in dataObj) {
+    dataObj[key] = await dataObj[key];
+  }
+  return dataObj;
+};
+
 // Categories related checkbox handler and support functions
 //
 // function to create checkedItemMap from an Array of objects
