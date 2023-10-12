b561b2cc6e0572431cdca3389b5af6ced0e17cd2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-12 23:36:56 +0200

    Add ErrorBoundary component

---
+++ src/ErrorBoundaries/ErrorBoundary.jsx
@@ -0,0 +1,25 @@
+import { useRouteError } from "react-router-dom";
+import {
+  Center,
+  Container,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
+
+export const ErrorBoundry = () => {
+  const error = useRouteError();
+  console.error("ErrorBoundry\n", error);
+  return (
+    <Center minH="100vh">
+      <Container size="lg">
+        <Alert status="error" display="flex" flexDirection="column">
+          <AlertIcon boxSize="69px" pb={4} />
+          <AlertTitle>The shit has hit the fan...!</AlertTitle>
+          <AlertDescription>{error.message}</AlertDescription>
+        </Alert>
+      </Container>
+    </Center>
+  );
+};
