5783323280f777278385779e5947b1808967431f
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-24 00:09:29 +0200

    Add custom required FormControl validatin on form submit

--- src/components/Forms/EditEventForm.jsx
+++ src/components/Forms/EditEventForm.jsx
@@ -18,6 +18,7 @@ import {
   setCheckedItemMap,
   createCheckedIdsArr,
   generateDateTimeStr,
+  checkFormDataOnSubimt,
 } from "../../util/globalFunctions";
 
 export const EditEventForm = ({ categories, event, onClose }) => {
@@ -25,7 +26,8 @@ export const EditEventForm = ({ categories, event, onClose }) => {
   const [isChecked, setIsChecked] = useState(
     setCheckedItemMap(initialCheckedItemMap, event.categoryIds)
   );
-
+  const [invalidInput, setInvalidInput] = useState({});
+  console.log("missingInput:", invalidInput);
   return (
     <Stack
       as={Form}
@@ -35,20 +37,34 @@ export const EditEventForm = ({ categories, event, onClose }) => {
       spacing={5}
       p={4}
       maxW="500px"
-      onSubmit={onClose}
+      onSubmit={(e) => {
+        const checkFormResult = checkFormDataOnSubimt(e);
+        console.log("on submit outer:", checkFormResult);
+        if (checkFormResult.isDataComplete) {
+          onClose();
+        } else if (checkFormResult.isRequiredOk) {
+          onClose();
+        } else {
+          e.preventDefault();
+          setInvalidInput(checkFormResult.invalid);
+        }
+      }}
     >
-      <FormControl isRequired>
+      <FormControl isInvalid={invalidInput.title}>
         <FormLabel>Title</FormLabel>
         <Input type="text" name="title" defaultValue={event.title} />
-        <FormErrorMessage></FormErrorMessage>
+        {invalidInput.title ? (
+          <FormErrorMessage>Event title is required</FormErrorMessage>
+        ) : null}
       </FormControl>
+
       <Stack as="fieldset" direction="column" spacing={2}>
         <Text as="legend">Date and Time</Text>
         <FormControl
           display={"flex"}
           flexDirection={"row"}
           alignItems={"center"}
-          isRequired
+          isInvalid={invalidInput.startTime}
         >
           <FormLabel margin={0} px={2}>
             Start
@@ -58,13 +74,15 @@ export const EditEventForm = ({ categories, event, onClose }) => {
             name="startTime"
             defaultValue={generateDateTimeStr(event.startTime)}
           />
-          <FormErrorMessage></FormErrorMessage>
+          {invalidInput.startTime ? (
+            <FormErrorMessage>Start time is required.</FormErrorMessage>
+          ) : null}
         </FormControl>
+
         <FormControl
           display={"flex"}
           flexDirection={"row"}
           alignItems={"center"}
-          isRequired
         >
           <FormLabel margin={0} px={2}>
             End
@@ -74,16 +92,15 @@ export const EditEventForm = ({ categories, event, onClose }) => {
             name="endTime"
             defaultValue={generateDateTimeStr(event.endTime)}
           />
-          <FormErrorMessage></FormErrorMessage>
+          <FormErrorMessage>End time is required.</FormErrorMessage>
         </FormControl>
       </Stack>
-      <FormControl isRequired>
+      <FormControl isInvalid={invalidInput.description}>
         <FormLabel>Description</FormLabel>
         <Textarea name="description" defaultValue={event.description} />
-        <FormErrorMessage></FormErrorMessage>
+        <FormErrorMessage>Event description is required.</FormErrorMessage>
       </FormControl>
-      <FormControl as={"fieldset"}>
-        {/* TODO: figure out how to make 1 of 3 required warning */}
+      <FormControl as={"fieldset"} isInvalid={invalidInput.categoryIds}>
         <Text as="legend">Categories</Text>
         <Stack spacing={[1, 5]} direction={["column", "row"]}>
           {categories.map((category) => (
@@ -93,20 +110,27 @@ export const EditEventForm = ({ categories, event, onClose }) => {
               name="categoryIds"
               isChecked={isChecked.get(category.id)}
               value={createCheckedIdsArr(isChecked)}
-              onChange={(e) =>
-                handleCheckboxChanges(e, isChecked, setIsChecked)
-              }
+              onChange={(e) => {
+                handleCheckboxChanges(e, isChecked, setIsChecked);
+              }}
             >
               {category.name}
             </Checkbox>
           ))}
         </Stack>
-        <FormErrorMessage></FormErrorMessage>
+        <FormErrorMessage>At least one category is required.</FormErrorMessage>
       </FormControl>
-      <FormControl>
+      <FormControl isInvalid={invalidInput.image}>
         <FormLabel>Add an image URL</FormLabel>
-        <Input type="url" name="image" defaultValue={event.image} overflow="" />
-        <FormErrorMessage></FormErrorMessage>
+        <Input
+          type="url"
+          name="image"
+          defaultValue={event.image}
+          errorBorderColor="orange.300"
+        />
+        <FormErrorMessage textColor="orange.500">
+          Your event would look nicer with an image, it is not required though.
+        </FormErrorMessage>
       </FormControl>
       <Stack direction="row" spacing={2} py={4} px={0} justifyContent="end">
         <Button type="submit" variant="ghost" size="lg" colorScheme="purple">
