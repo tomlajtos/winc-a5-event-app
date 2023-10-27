dd5f0ee86c333464713073a006cf1cfd16e02982
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-27 22:29:45 +0200

    refactor: Use new checkb. e. handler 'handleFilterChange', new checked state management
    refactor: remove unused import

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Link as RRLink } from "react-router-dom";
 import {
   Accordion,
@@ -15,27 +15,8 @@ import {
 
 import { RootContext } from "../context/RootContext";
 
-import {
-  handleCheckboxChanges,
-  initCheckedItemMap,
-} from "../util/globalFunctions";
-
 export const Navigation = () => {
   const { categories, filterQ, setFilterQ } = useContext(RootContext);
-  const checkedCategoriesMap = new Map(initCheckedItemMap(categories, true));
-  const [isChecked, setIsChecked] = useState(
-    new Map([...checkedCategoriesMap])
-  );
-  // console.log(
-  //   "nav init map\n",
-  //   checkedCategoriesMap,
-  //   "\n",
-  //   "nav checked map\n",
-  //   isChecked,
-  //   "\n",
-  //   "nav filterQ:\n",
-  //   filterQ,
-  // );
 
   return (
     <Stack
@@ -89,18 +70,36 @@ export const Navigation = () => {
                   id={category.id}
                   key={category.id}
                   name="categoryIds"
-                  isChecked={isChecked.get(category.id)}
+                  isChecked={filterQ.includes(category.id)}
                   value={category.id}
                   colorScheme="purple"
-                  onChange={(e) =>
-                    handleCheckboxChanges(
-                      e,
-                      isChecked,
-                      setIsChecked,
-                      setFilterQ
-                    )
-                  }
-                  checked
+                  onChange={(e) => {
+                    // TODO: move to utils, add jsDOC comments
+                    const handleFilterChange = (
+                      filters,
+                      filterInput,
+                      setFn
+                    ) => {
+                      let newFilters = [...filters];
+                      console.log(
+                        "%c onChange NAV > filter",
+                        "color:red;background:white"
+                      );
+                      console.log("newFilters before", newFilters);
+                      if (e.target.checked) {
+                        newFilters = Array.from(
+                          new Set([Number(filterInput.value), ...newFilters])
+                        );
+                      } else {
+                        newFilters = newFilters.filter(
+                          (q) => q !== Number(filterInput.value)
+                        );
+                      }
+                      console.log("newFilters after", newFilters);
+                      setFn(newFilters);
+                    };
+                    handleFilterChange(filterQ, e.target, setFilterQ);
+                  }}
                 >
                   {category.name}
                 </Checkbox>
