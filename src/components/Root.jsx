be0c871c546d6addc8777b2b1580037510dd29b2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-11-11 01:56:09 +0100

    refactor: use separate Header component

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,14 +1,12 @@
 // React and RRouter imports
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet, useLoaderData } from "react-router-dom";
 // Chakra-ui imports
 import { Box, Flex, Heading } from "@chakra-ui/react";
 // Context imports
 import { RootContext } from "../context/RootContext";
 // Component imports
-// import { Navigation } from "./Navigation";
-import { Navigation } from "./Navigation";
-import { Search } from "../components/Search";
+import { Header } from "../components/Header";
 // utils imports
 import { fetchData, initCategoryIdsArr } from "../util/globalFunctions";
 
@@ -23,9 +21,9 @@ export const loader = async () => {
 export const Root = () => {
   const { categories, users } = useLoaderData();
   const categoryIds = initCategoryIdsArr(categories);
+
   const [searchQ, setSearchQ] = useState("");
   const [filterQ, setFilterQ] = useState([...categoryIds]);
-  const [headerHeight, setHeaderHeight] = useState(0);
   const header = useRef();
 
   useEffect(() => {
@@ -40,9 +38,12 @@ export const Root = () => {
 
   return (
     <Flex
-      direction={"column"}
-      minH={"100vh"}
-      margin="auto"
+    <Box
+      className="root-container"
+      mx={0}
+      px={0}
+      b={8}
+      h={"100vh"}
       background="gray.200"
       alignItems="center"
       pb={8}
@@ -58,52 +59,12 @@ export const Root = () => {
           headerHeight,
         }}
       >
-        <Flex
-          as="header"
-          ref={header}
-          width="full"
-          px={[2, 4, 8, null, 12]}
-          py={6}
-          position="sticky"
-          top={0}
-          zIndex="sticky"
-          wrap="wrap"
-          direction={["column", null, "row"]}
-          gap={2}
-          background="gray.800"
-          minH={"6vh"}
-          align="center"
-          onMouseOver={(e) => console.log(e)}
-        >
-          <Flex
-            direction={["column", null, "row"]}
-            flex={1}
-            justify="space-between"
-          >
-            <Heading
-              as="h1"
-              // NOTE: if lineH. is not def, it will change on on sm/md breakpoint for some reason
-              // lineHeight="3rem"
-              fontSize="3rem"
-              width="fit-content"
-              color="purple.200"
-              fontWeight="light"
-              fontFamily="mono"
-              pr={8}
-              pl={[8, null, 0]}
-            >
-              EventApp
-            </Heading>
-            <Search />
-          </Flex>
-          <Navigation />
-        </Flex>
-        {headerHeight && (
-          <Box pt={0} backgroundColor="transparent" mx="auto">
-            <Outlet />
-          </Box>
-        )}
+        <Header />
+        <Box pt={0} backgroundColor="transparent" mx="auto">
+          <Outlet />
+        </Box>
+        {/* )} */}
       </RootContext.Provider>
-    </Flex>
+    </Box>
   );
 };
