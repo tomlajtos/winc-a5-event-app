f1b94935fbd2f564acf3c9e8cdfe08f2c7237ad8
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-13 18:00:06 +0100

    fix: prevent returning undefined, validate response
    - set initial data state to `[]`
    - validate response.ok > do not return response.json() if not .ok
      (triggers error boundary in case of !response.ok and leaves state as `[]`)
    - remove catch from end of chain - it is ignored by react

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -2,14 +2,17 @@ import { useState, useEffect } from "react";
 
 export const useData = (url) => {
   const baseUrl = "http://localhost:3003";
-  const[data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch(`${baseUrl}${url}`)
-      .then((response) => response.json())
-      .then((json) => {
-          setData(json);
-      }).catch(error=>error.message);
-  },[]);
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((json) => setData(json));
+  }, []);
+
   return data;
 };
