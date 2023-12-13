b8faee3fb3bb6603248e19f23c81810a11e06465
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-13 18:11:00 +0100

    refactor(search): Add ModalSearch.jsx move modal as separate component
    - move the entire modal into it's own component
    - use CompactEventList component
    - prop drilling from search to be replaced with context in the future

---
+++ src/components/search/ModalSearch.jsx
@@ -0,0 +1,66 @@
+import {
+  Input as CInput,
+  Modal,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  ModalOverlay,
+  Spacer,
+} from "@chakra-ui/react";
+
+import { CompactEventList } from "./CompactEventList";
+import { Logger } from "../../util/Logger";
+
+export const ModalSearch = ({
+  inputRef,
+  isOpen,
+  onClose,
+  searchQ,
+  setSearchQ,
+  categories,
+}) => {
+  return (
+    <Modal
+      size={["full", null, "xl"]}
+      initialFocusRef={inputRef}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <Logger type="render" name="SearchModal" color="cyan" bg="black" />
+      <ModalOverlay
+        backdropFilter="auto"
+        backdropBlur="50px"
+        backdropSaturate="500%"
+      />
+      <ModalContent bg="whiteAlpha.300">
+        <ModalCloseButton size="sm" />
+        <ModalBody px={[0, 0, 4]} py={10} align="center">
+          <CInput
+            ref={inputRef}
+            w={["330px"]}
+            name="search"
+            variant="outline"
+            type="input"
+            aria-label="popup-search"
+            placeholder="Type event title..."
+            _placeholder={{ color: "gray.300" }}
+            rounded="xl"
+            borderColor={"gray.300"}
+            color="gray.200"
+            focusBorderColor="purple.300"
+            defaultValue={searchQ}
+            onChange={(e) => {
+              setSearchQ(e.target.value);
+            }}
+          />
+          <Spacer h={6} />
+          <CompactEventList
+            categories={categories}
+            searchQ={searchQ}
+            onClose={onClose}
+          />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
