478cea744dd26bdd240e892f376cbeb1a8391949
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-13 18:05:48 +0100

    refactor(search): Add CompactEventList.jsx move search modal results
    - separate component to render a list of small event cards as search results
    - fetch events here (temporary)

---
+++ src/components/search/CompactEventList.jsx
@@ -0,0 +1,26 @@
+import { Link as RRLink } from "react-router-dom";
+import { Stack, StackItem } from "@chakra-ui/react";
+import { EventCardSmall } from "../EventCardSmall";
+import { useData } from "../../hooks/useData";
+
+export const CompactEventList = ({ searchQ, onClose, categories }) => {
+  const events = useData("/events");
+
+  const searchResults = events.filter(
+    (e) =>
+      e.title.toLowerCase().includes(searchQ.toLowerCase()) &&
+      searchQ.length > 0,
+  );
+
+  return (
+    <Stack>
+      {searchResults.map((event) => (
+        <StackItem key={event.id}>
+          <RRLink to={`/event/${event.id}`} onClick={onClose}>
+            <EventCardSmall event={event} categories={categories} />
+          </RRLink>
+        </StackItem>
+      ))}
+    </Stack>
+  );
+};
