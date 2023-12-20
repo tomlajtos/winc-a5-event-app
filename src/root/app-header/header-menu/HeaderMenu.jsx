cece465d25223c37b5c190ed85b0b78e0d76f490
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-20 13:57:46 +0100

    Add HeaderMenu component

---
+++ src/root/app-header/header-menu/HeaderMenu.jsx
@@ -0,0 +1,22 @@
+// React and RRouter imports
+import React, { useContext } from "react";
+// Chakra-ui imports
+import { Flex } from "@chakra-ui/react";
+// Context imports
+import { RootContext } from "../../../context/RootContext";
+// Component imports
+import { NavMenu } from "./navigation/NavMenu";
+import { Navigation } from "./navigation/Navigation";
+import { Search } from "./search/Search";
+
+export const HeaderMenu = () => {
+  const { rootSize } = useContext(RootContext);
+
+  return (
+    <Flex align="center" gap={6}>
+      {rootSize.width > 767 && <Search justifySelf="center" order={1} />}
+      {rootSize.width > 1023 && <Navigation />}
+      {rootSize.width < 1024 && <NavMenu />}
+    </Flex>
+  );
+};
