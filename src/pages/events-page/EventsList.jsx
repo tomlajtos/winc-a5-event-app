5180d770fbee342432568b1cc09e14df04d3a872
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-21 12:56:33 +0100

    refactor: Add and use separate EventsList comp to render EventCard-s

---
+++ src/pages/events-page/EventsList.jsx
@@ -0,0 +1,53 @@
+// React imports
+import React, { useContext } from "react";
+// React Router imports
+import { Link as RRLink } from "react-router-dom";
+// ChakraUi imports
+import { Wrap } from "@chakra-ui/react";
+// Context and custom hook imports
+import { RootContext } from "../../context/RootContext";
+// App component imports
+import { EventCard } from "./EventCard";
+// Util imports
+// import { fetchData } from "../../util/fetch.js";
+// import { log } from "../../util/Logger";
+
+export const EventsList = ({ events }) => {
+  const { filters, searchQ, rootSize } = useContext(RootContext);
+
+  const eventsToRender = events
+    .filter((event) => {
+      if (!filters.length) {
+        return null;
+      } else if (event.categoryIds.some((id) => filters.includes(id))) {
+        return event;
+      }
+    })
+    .filter((event) => {
+      if (searchQ === "") {
+        return events;
+      } else if (event.title.toLowerCase().includes(searchQ.toLowerCase())) {
+        return event;
+      }
+    });
+
+  const wrapHeight = rootSize.height - 184; // 184 <-- Wrap.paddingY + PageTitle.height + AppHeader.height
+
+  return (
+    <Wrap
+      direction={"row"}
+      height={`${wrapHeight}px`}
+      justify="center"
+      spacing={4}
+      py={6}
+      px={[2, 4, 4, null, 12]}
+      overflowY="scroll"
+    >
+      {eventsToRender.map((event) => (
+        <RRLink key={event.id} to={`/event/${event.id}`}>
+          <EventCard event={event} />
+        </RRLink>
+      ))}
+    </Wrap>
+  );
+};
--- src/pages/events-page/EventsPage.jsx
+++ src/pages/events-page/EventsPage.jsx
@@ -1,46 +1,25 @@
 // React imports
-import React, { useContext } from "react";
+import React from "react";
 // React Router imports
-import { useLoaderData, Link as RRLink } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 // ChakraUi imports
-import { Box, Heading, Wrap } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 // Context and custom hook imports
-import { RootContext } from "../../context/RootContext";
 // App component imports
-import { EventCard } from "./EventCard";
+import { EventsList } from "./EventsList";
 // Util imports
 import { fetchData } from "../../util/fetch.js";
-import { log } from "../../util/Logger";
+import { Logger } from "../../util/Logger";
 
 export const loader = async () =>
   fetchData([{ name: "events", path: "/events" }]);
 
 export const EventsPage = () => {
   let { events } = useLoaderData();
-  const { filters, searchQ, rootSize } = useContext(RootContext);
-
-  const eventsToRender = events
-    .filter((event) => {
-      if (!filters.length) {
-        return null;
-      } else if (event.categoryIds.some((id) => filters.includes(id))) {
-        return event;
-      }
-    })
-    .filter((event) => {
-      if (searchQ === "") {
-        return events;
-      } else if (event.title.toLowerCase().includes(searchQ.toLowerCase())) {
-        return event;
-      }
-    });
-
-  const wrapHeight = rootSize.height - 184; // 184 <-- Wrap[py] + Heading.height + Header.heigh
-
-  log.comp("EventsPage", "navy", "yellow");
 
   return (
     <Box className="events-page-container">
+      <Logger type="render" name="EventsPage" color="navy" bg="yellow" />
       <Heading
         display="box"
         height="65px"
@@ -53,21 +32,7 @@ export const EventsPage = () => {
       >
         Events
       </Heading>
-      <Wrap
-        direction={"row"}
-        height={`${wrapHeight}px`}
-        justify="center"
-        spacing={4}
-        py={6}
-        px={[2, 4, 4, null, 12]}
-        overflowY="scroll"
-      >
-        {eventsToRender.map((event) => (
-          <RRLink key={event.id} to={`/event/${event.id}`}>
-            <EventCard event={event} />
-          </RRLink>
-        ))}
-      </Wrap>
+      <EventsList events={events} />
     </Box>
   );
 };
