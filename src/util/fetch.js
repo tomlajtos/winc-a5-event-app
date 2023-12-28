7bb497e7e789fd9226b08708e54c2917196f0dc5
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-28 14:21:50 +0100

    refactor(getData): rename setCallback to setState

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,28 +1,32 @@
+// TODO: thrown errors are ignored, only component level error is picked up by error boundaries and React Router
+// find a solution if possible
+
 // TODO: add jsDOC comments
 // TODO: use less complicated fetching for single resource only
-export const getData = async (endpointPath, setCallback) => {
+export const getData = async (endpointPath, setState) => {
   const baseUrl = "http://localhost:3003";
   const promise = await fetch(`${baseUrl}${endpointPath}`);
   // console.log(promise);
   if (promise.ok) {
     let data = await promise.json();
-    // console.log("data in getData", data);
-    setCallback(data);
+    console.log("data in getData", data);
+    setState(data);
   } else {
     console.warn(promise);
-    throw new Error(`${promise.status} (${promise.statusText})`);
+    throw new Error(`${promise.status}:(${promise.statusText})`);
   }
 };
 
 // TODO: add jsDOC comments
 export const getMultiData = (endpointArr) => {
   //example: [{  path: "/categories", setCallback: setCategories}]
-  endpointArr.map((endpoint) => getData(endpoint.path, endpoint.setCallback));
+  endpointArr.map((endpoint) => getData(endpoint.path, endpoint.setState));
 };
 
 // TODO: Revisit: I'm note sure if I like this
 // helper function for React Router loader function
-// TODO: check if mixing async/await with promise chaining is OK,... probably not >> FIX
+// FIX: do not mix async/await and promise chaining
+// TODO: replace this with a `getMultiDataObj` or similar
 export const fetchData = async (
   endpoints = [{ name: "categories", path: "/categories" }],
 ) => {
