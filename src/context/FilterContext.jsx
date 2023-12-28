dd5af8a0d09be9160e667afc9fb9ab6f39f2dcce
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-28 13:30:13 +0100

    Add FilterContext.jsx
    exports custom FilterContextProvider
    exports custom useFilters hook
    provided values: filters, setFilters

---
+++ src/context/FilterContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+import { Logger } from "../util/Logger";
+
+export const FilterContext = createContext({});
+FilterContext.displayName = "FilterContext";
+
+export const FilterContextProvider = ({ children, categoryIds }) => {
+  const [filters, setFilters] = useState([...categoryIds]);
+
+  return (
+    <FilterContext.Provider value={{ filters, setFilters }}>
+      <Logger
+        type="render"
+        name="Filter.Context.Provider"
+        color="yellow"
+        bg="purple"
+      />
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilters = () => {
+  const context = useContext(FilterContext);
+  if (!context) {
+    throw new Error("useStaticData must be used within FilterContext");
+  }
+  return context;
+};
