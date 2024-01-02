cd73d08c50397a31ce4626ff992d7eeac7ca6312
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-01-02 22:20:10 +0100

    refactor: move Form to its own separate component > NewEventForm

---
+++ src/components/forms/NewEventForm.jsx
@@ -0,0 +1,111 @@
+// React and RRouter imports
+// import { useState } from "react";
+import { Form } from "react-router-dom";
+
+// Context imports
+import { useStaticData } from "../../context/StaticDataContext.jsx";
+// chakra-ui imports
+import { Stack, Text } from "@chakra-ui/react";
+// component imports
+import { TextInputControl } from "./form-controlls/TextInputControl";
+import { DateTimeControl } from "./form-controlls/DateTimeControl";
+import { TextareaControl } from "./form-controlls/TextareaControl";
+import { CheckboxGrControl } from "./form-controlls/CheckboxGrControl";
+import { UrlInputControl } from "./form-controlls/UrlInputControl";
+import { SelectControl } from "./form-controlls/SelectControl";
+import { SaveNewButton } from "./buttons/SaveNewButton.jsx";
+// util imports
+import { generateDateTimeStr } from "../../util/datetime.js";
+
+export const NewEventForm = ({ stateProps }) => {
+  const { categories, users } = useStaticData();
+
+  return (
+    <Stack
+      as={Form}
+      id="new-event-form"
+      method="post"
+      direction="column"
+      spacing={5}
+      mx="auto"
+      py={8}
+      px={[4, 8]}
+      maxWidth={["full", "container.sm"]}
+    >
+      <TextInputControl
+        label="Title"
+        inputName="title"
+        defaultValue=""
+        isRequired={true}
+        {...stateProps}
+      />
+      {/* SELECT for event creator(users)*/}
+      <SelectControl
+        label="Created by"
+        inputName="createdBy"
+        defaultValue={"Phantom of The EventApp"}
+        isRequired={true}
+        users={users}
+        {...stateProps}
+      />
+      {/* FIELDSET for start/endTime */}
+      <Stack as="fieldset" direction="column" spacing={2}>
+        <Text as="legend" pb={1} fontWeight="bolder">
+          Date and Time
+        </Text>
+        {/* INPUT for startTime */}
+        <DateTimeControl
+          label="Start"
+          inputName="startTime"
+          defaultValue={generateDateTimeStr("current")}
+          isRequired={true}
+          {...stateProps}
+        />
+        {/* INPUT for endTime */}
+        <DateTimeControl
+          label="End"
+          inputName="endTime"
+          defaultValue={generateDateTimeStr("current", { h: 1, m: 0 })}
+          isRequired={true}
+          {...stateProps}
+        />
+      </Stack>
+      {/* INPUT for location */}
+      <TextInputControl
+        label="Location"
+        inputName="location"
+        defaultValue=""
+        isRequired={true}
+        {...stateProps}
+      />
+      {/* INPUT for description */}
+      <TextareaControl
+        label="Description"
+        inputName="description"
+        defaultValue={event.description}
+        isRequired={true}
+        {...stateProps}
+      />
+      {/* CHECKBOX GR. for categories */}
+      <CheckboxGrControl
+        grTitle="Categories"
+        showAsRequired={true}
+        inputName="categoryIds"
+        categories={categories}
+        {...stateProps}
+      />
+      {/* INPUT for image(url) */}
+      <UrlInputControl
+        label="Add an image URL"
+        inputName="image"
+        defaultValue={event.image}
+        isRequired={false}
+        {...stateProps}
+      />
+      <SaveNewButton
+        categoryIds={stateProps.categoryIds}
+        setErrors={stateProps.setErrors}
+      />
+    </Stack>
+  );
+};
--- src/pages/NewEventPage.jsx
+++ src/pages/NewEventPage.jsx
@@ -2,27 +2,19 @@
 
 // React and RRouter imports
 import { useState } from "react";
-import { redirect, Form } from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 //Context imports
-import { useStaticData } from "../context/StaticDataContext.jsx";
 
 //chakra-ui imports
-import { Box, Container, Flex, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Stack } from "@chakra-ui/react";
 
 // component imports
 import { PageTitle } from "./PageTitle";
-import { TextInputControl } from "../components/forms/form-controlls/TextInputControl";
-import { DateTimeControl } from "../components/forms/form-controlls/DateTimeControl";
-import { TextareaControl } from "../components/forms/form-controlls/TextareaControl";
-import { CheckboxGrControl } from "../components/forms/form-controlls/CheckboxGrControl";
-import { UrlInputControl } from "../components/forms/form-controlls/UrlInputControl";
-import { SelectControl } from "../components/forms/form-controlls/SelectControl";
 import { SaveNewButton } from "../components/forms/buttons/SaveNewButton";
-import { CancelNewButton } from "../components/forms/buttons/CancelNewButton";
-
+import { CancelNewButton } from "../components/forms/buttons/CancelNewButton.jsx";
+import { NewEventForm } from "../components/forms/NewEventForm.jsx";
 // util imports
-import { generateDateTimeStr } from "../util/datetime.js";
 import { Logger } from "../util/Logger.jsx";
 
 export const action = async ({ request }) => {
@@ -44,7 +36,6 @@ export const action = async ({ request }) => {
 };
 
 export const NewEventPage = () => {
-  const { categories, users } = useStaticData();
   const [categoryIds, setCategoryIds] = useState([]);
   const [inputErrors, setInputErrors] = useState(new Map());
   const stateProps = {
@@ -71,97 +62,14 @@ export const NewEventPage = () => {
         borderColor="gray.300"
       >
         <PageTitle title="New Event" position="sticky" top="95" border="none" />
+
         {/* form button group */}
         <Stack direction="row" spacing={2} pr={8} justifyContent="end">
           <SaveNewButton categoryIds={categoryIds} setErrors={setInputErrors} />
           <CancelNewButton />
         </Stack>
       </Flex>
-
-      <Container
-        as={Form}
-        method="post"
-        id="new-event-form"
-        mx="auto"
-        pt={6}
-        width={["full", "sm", "container.sm"]}
-        direction="column"
-        alignItems="center"
-      >
-        <Stack direction="column" spacing={5} p={4} with="full">
-          {/* INPUT for title */}
-          <TextInputControl
-            label="Title"
-            inputName="title"
-            defaultValue=""
-            isRequired={true}
-            {...stateProps}
-          />
-          {/* SELECT for event creator(users)*/}
-          <SelectControl
-            label="Created by"
-            inputName="createdBy"
-            defaultValue={"Phantom of The EventApp"}
-            isRequired={true}
-            users={users}
-            {...stateProps}
-          />
-          {/* FIELDSET for start/endTime */}
-          <Stack as="fieldset" direction="column" spacing={2}>
-            <Text as="legend" pb={1} fontWeight="bolder">
-              Date and Time
-            </Text>
-            {/* INPUT for startTime */}
-            <DateTimeControl
-              label="Start"
-              inputName="startTime"
-              defaultValue={generateDateTimeStr("current")}
-              isRequired={true}
-              {...stateProps}
-            />
-            {/* INPUT for endTime */}
-            <DateTimeControl
-              label="End"
-              inputName="endTime"
-              defaultValue={generateDateTimeStr("current", { h: 1, m: 0 })}
-              isRequired={true}
-              {...stateProps}
-            />
-          </Stack>
-          {/* INPUT for location */}
-          <TextInputControl
-            label="Location"
-            inputName="location"
-            defaultValue=""
-            isRequired={true}
-            {...stateProps}
-          />
-          {/* INPUT for description */}
-          <TextareaControl
-            label="Description"
-            inputName="description"
-            defaultValue={event.description}
-            isRequired={true}
-            {...stateProps}
-          />
-          {/* CHECKBOX GR. for categories */}
-          <CheckboxGrControl
-            grTitle="Categories"
-            showAsRequired={true}
-            inputName="categoryIds"
-            categories={categories}
-            {...stateProps}
-          />
-          {/* INPUT for image(url) */}
-          <UrlInputControl
-            label="Add an image URL"
-            inputName="image"
-            defaultValue={event.image}
-            isRequired={false}
-            {...stateProps}
-          />
-        </Stack>
-      </Container>
+      <NewEventForm stateProps={stateProps} />
     </Box>
   );
 };
