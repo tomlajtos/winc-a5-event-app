67c33408122b634c33348c3cdc107f3daee5ae76
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-01-08 23:12:18 +0100

    Add EventCreator.jsx

---
+++ src/pages/event-page/EventCreator.jsx
@@ -0,0 +1,29 @@
+import { Avatar, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { useStaticData } from "../../context/StaticDataContext";
+import phantom from "../../assets/phantom_mask.svg";
+import { Logger } from "../../util/Logger";
+export const EventCreator = ({ event }) => {
+  const { users } = useStaticData();
+  const [user] = users.filter((user) => user.id === Number(event.createdBy));
+  return (
+    <Stack>
+      <Logger type="render" target="component" name="Event Creator" level={4} />
+      <Heading size={"md"}>Created by:</Heading>
+      {user ? (
+        <Flex gap={4} alignItems="center">
+          <Avatar size="lg" name={user.name} src={user.image} />
+          <Text fontSize="2xl">{user.name}</Text>
+        </Flex>
+      ) : (
+        <Flex py={2} direction="row" align="center" gap={2}>
+          <Avatar
+            size="md"
+            name="Unknown Person"
+            src={event.createdBy ? phantom : null}
+          />
+          <Text>{event.createdBy ? event.createdBy : "Unknown Person"}</Text>
+        </Flex>
+      )}
+    </Stack>
+  );
+};
