62c264ed4d254ade60a4dd22eaf550dcf01b7e17
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-01-10 11:41:29 +0100

    Remove useMultiDataObj.js > unnecessary

--- src/hooks/useMultiDataObj.js
+++
@@ -1,7 +0,0 @@
-import {useData} from "./useData"
-
-export const useMultiDataObj = (endpoints) => {
-  const dataObj= endpoints.reduce(( res,endpoint ) => ({...res,[endpoint.name]:useData(endpoint.path)}),{})
-
-  return dataObj
-}
