7f60df50a3a7b1cf77dedab07133740f9e287c40
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-01-13 16:38:06 +0100

    refactor: use simpler stack fromatting add transformeError function
    - no need to get individual data items out of stackline
    - add transformError function and newProps obj to generate
      a new valid Error object with default values for missing data

--- src/util/error.js
+++ src/util/error.js
@@ -1,82 +1,117 @@
-const getStackLineData = (line) => {
-  const locationRegX = /((\d+)?\S?\d+)(?=\))/g;
-  const nameRegX = /(\w+)\.?(\w+)(?=\s\()/g;
-  const fileNameRegX = /([\w-_]+)\.(\w+)(?=\?)/g;
-  const parenthBlockRegX = /\(.+[)}]/g;
+import { log } from "./log";
 
-  const [textObj] = line.match(nameRegX);
-  const isAsync = line.includes("async") ? "(async)" : "";
-  const fileName = line.match(fileNameRegX) ? line.match(fileNameRegX)[0] : "-";
-  const parenthBlock = line.match(parenthBlockRegX);
+export const transformError = (error) => {
+  console.warn("transforming error");
 
-  // console.log(
-  //   "textObject:",
-  //   textObj,
-  //   "\nisAsync:",
-  //   isAsync,
-  //   "\nfileName:",
-  //   fileName,
-  //   "\nparenthBlock",
-  //   parenthBlock,
-  // );
-  let [errLocation] = locationRegX.test(line)
-    ? line.match(locationRegX)
-    : line.match(parenthBlockRegX);
-  console.log("errLocation:", errLocation);
-  // const [lineNum, colNum] = errLocation.split(":");
-  const fullLine = `at ${textObj} ${isAsync} of ${fileName} >> loc: ${errLocation}`;
-  console.log("fullLine", fullLine);
-  console.log("***");
-  return {
-    async: isAsync,
-    textObject: textObj,
-    fileName,
-    location: errLocation,
-    line: fullLine,
-    // lineNum,
-    // colNum,
-    // line: fullLine
-  };
+  const { message, name, status, statusText, stack, url, data } = error;
+
+  const newError = new Error("new");
+
+  // order in newProps arr matters when creating a new Error!
+  const newProps = [
+    ["name", name ? name : data ? data.name : "Error (fallback value)"],
+    [
+      "status",
+      status
+        ? status
+        : data
+          ? data.status
+          : "Unknown error status (fallback value)",
+    ],
+    [
+      "statusText",
+      statusText
+        ? statusText
+        : data
+          ? data.statusText
+          : "The error status has no description (fallback value)",
+    ],
+    [
+      "message",
+      message
+        ? message
+        : data
+          ? data.message
+          : "Something went wrong (fallback value)",
+    ],
+    [
+      "stack",
+      stack && /\\n/.test(stack)
+        ? stack
+        : data
+          ? data.resStack || data.stack
+          : `${error.name}: ${error.statusText} - ${error.status}\n  This error does not provide any details.`,
+    ],
+    ["url", url ? url : data ? data.url : ""],
+    ["data", data ? data : {}],
+  ];
+
+  newProps.map((prop) => (newError[prop[0]] = prop[1]));
+  return newError;
 };
-// const getStackLineData = (stackLine) => {
-//   console.log("getStackLineData > stackLine:\n", stackLine);
-//   const splitLine = stackLine.split(" ").filter((str) => str.length > 0);
-//   const splitLine2 = chewLine(stackLine);
-//   // console.log("getStackLineData > splitLine:", splitLine);
-//   console.log("getStackLineData > splitLine2:", splitLine2);
-//
-//   const fileName = splitLine.at(-2);
-//   console.log("getStackLineData > fileName:", fileName);
-//   const [lineNum] = splitLine.at(-1).match(/\d+(?=:\d+\))/g);
-//   console.log("getStackLineData > lineNum:", lineNum);
-//   const [colNum] = splitLine.at(-1).match(/\d+(?=\))/g);
-//   console.log("getStackLineData > colNum:", colNum);
-//   const prettyLine = `at ${fileName} >> (${lineNum}:${colNum})`;
-//   console.log("getStackLineData > prettyLine:", prettyLine);
-//   return { fileName, lineNum, colNum, line: prettyLine };
-// };
 
-export const pretifyError = (error) => {
-  console.log("prettifyError > error:", error, { error });
-  const stackLinesArr = error.stack.split("\n").slice(1);
-  console.log("prettifyError > stackLinesArr:", stackLinesArr);
+export const prettifyError = (error) => {
+  const stackLinesArr = error.stack.split("\n");
 
-  const stackLineObjects = stackLinesArr.map((line) => {
-    console.log("stackLineObjects > line:\n", line);
-    console.log("stackLineObjects > getSteckLineData:", getStackLineData(line));
-    return getStackLineData(line);
-  });
-  console.log("stackLineObjects arr:", stackLineObjects);
-  return {
+  // not a valid Error object
+  const prettyErrorObj = {
     name: error.name,
+    status: error.status,
+    statusText: error.statusText,
     message: error.message,
-    stackLines: stackLineObjects,
+    stackLines: stackLinesArr,
+    data: error.data,
   };
+
+  return prettyErrorObj;
 };
 
 export const testError = {
-  name: "Tesat Error",
-  message: "This is a rest error",
+  name: "Test Error",
+  message: "This is a test error",
   stack:
-    "prettifyError > error: Error: Catch this you SOB...\nat fetchData (fetch.js?t=1704450514773:44:11)\nat async callLoaderOrAction (react-router-dom.js?v=b81d899d:2632:16)\nat async Promise.all (:3001/event/index 0)\nat async callLoadersAndMaybeResolveData (react-router-dom.js?v=b81d899d:1985:19)\nat async handleLoaders (react-router-dom.js?v=b81d899d:1645:9)\nat async startNavigation (react-router-dom.js?v=b81d899d:1502:9)\nat async startRedirectNavigation (react-router-dom.js?v=b81d899d:1975:7) {error: Error: Catch this you SOB...\nat fetchData (http://localhost:3001/src/util/fetch.js?t=1704450514…}",
+    "prettifyError > error: Error: Catch me if you can...\nat fetchData (fetch.js?t=1704450514773:44:11)\nat async callLoaderOrAction (react-router-dom.js?v=b81d899d:2632:16)\nat async Promise.all (:3001/event/index 0)\nat async callLoadersAndMaybeResolveData (react-router-dom.js?v=b81d899d:1985:19)\nat async handleLoaders (react-router-dom.js?v=b81d899d:1645:9)\nat async startNavigation (react-router-dom.js?v=b81d899d:1502:9)\nat async startRedirectNavigation (react-router-dom.js?v=b81d899d:1975:7) {error: Error: Catch this you SOB...\nat fetchData (http://localhost:3001/src/util/fetch.js?t=1704450514…}",
 };
+
+// deprecated code
+//
+// const getStackLineData = (line) => {
+//   const locationRegX = /((\d+)?\S?\d+)(?=\))/g;
+//   const nameRegX = /(\w+)\.?(\w+)(?=\s\()/g;
+//   const fileNameRegX = /([\w-_]+)\.(\w+)(?=\?)/g;
+//   const parenthBlockRegX = /\(.+[)}]/g;
+//
+//   const [textObj] = line.match(nameRegX) || ["unknown"];
+//   const isAsync = line.includes("async") ? "(async)" : "";
+//   const fileName = line.match(fileNameRegX)
+//     ? line.match(fileNameRegX)[0]
+//     : "unknown file";
+//
+//   const [errLocation] =
+//     locationRegX.test(line) && line.match(locationRegX)[0] !== 0
+//       ? line.match(locationRegX)
+//       : line.match(parenthBlockRegX) || ["unknown location"];
+//   const fullLine = `at ${textObj} ${isAsync} of ${fileName} >> loc: ${errLocation}`;
+//   return {
+//     async: isAsync,
+//     textObject: textObj,
+//     fileName,
+//     location: errLocation,
+//     line: fullLine,
+//   };
+// };
+
+// const arrayFromStackLines = (error) => {
+//   let stack = "";
+//   if (error.stack) {
+//     stack = error.stack;
+//     console.log("error has a stack prop", error.stack);
+//   } else if (error.data) {
+//     stack = error.data.stack;
+//     console.log(error.data.stack);
+//   } else {
+//     stack =""
+//   }
+//   log.value("STACK", stack);
+//   return stack.split("\n");
+// };
