61a2e4f52e009441dee3a0eb16570a456c590bab
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-01-13 16:22:24 +0100

    refactor: add separate overrides object and use styles
    - import and use styles for global style overrides
    - wrap all overrides in overrides object

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,8 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
+// global style overrides
+import { styles } from "./styles";
+// component style overrides
 import { buttonTheme } from "./components/button";
 
-export const theme = extendTheme({
+const overrides = {
+  styles,
   components: {
     Button: buttonTheme,
   },
-});
+};
+
+export const theme = extendTheme(overrides);
