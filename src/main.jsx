d5f61ac06262bd4470af183ad64d4ede224ddf47
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-01-29 03:54:46 +0100

    refactor: lazy load event and newEvent routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 // React imports
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 // React Router imports
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // Chakra-Ui imports
@@ -10,54 +9,72 @@ import { theme } from "./styles/index";
 // Context imports
 // App component imports
 import { Root, loader as rootLoader } from "./root/Root";
-import { GlobalLayout } from "./root/GlobalLayout.jsx";
+import { GlobalLayout, loader as eventsLoader } from "./root/GlobalLayout.jsx";
+
 import { ErrorBoundary } from "./error-boundaries/ErrorBoundary";
-import {
-  EventsPage,
-  loader as eventsLoader,
-} from "./pages/events-page/EventsPage";
+import { EventsPage } from "./pages/events-page/EventsPage";
 
-import { EventPage, loader as eventLoader } from "./pages/EventPage";
-import { NewEventPage, action as createEvent } from "./pages/NewEventPage";
-import { action as eventActions } from "./io/mutate.js";
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      id: "root",
+      element: <Root />,
+      loader: rootLoader,
+      errorElement: <ErrorBoundary />,
+      children: [
+        {
+          id: "globalLayout",
+          element: <GlobalLayout />,
+          // eventsLoader makes more sence here than in events page
+          loader: eventsLoader,
+          errorElement: <ErrorBoundary />,
+          children: [
+            {
+              id: "events",
+              element: <EventsPage />,
+              // set as index element so it renders in path: "/"
+              index: true,
+            },
+            {
+              path: "/event/:eventId",
+              id: "event",
+              async lazy() {
+                let { EventPage, loader } = await import("./pages/EventPage");
+                let { action } = await import("./io/mutate");
+                return {
+                  loader: loader,
+                  action: action,
+                  Component: EventPage,
+                };
+              },
+            },
+            {
+              path: "/event/new",
+              id: "newEvent",
+              async lazy() {
+                let { NewEventPage, action } = await import(
+                  "./pages/NewEventPage"
+                );
+                return {
+                  action: action,
+                  Component: NewEventPage,
+                };
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    id: "root",
-    element: <Root />,
-    loader: rootLoader,
-    errorElement: <ErrorBoundary />,
-    children: [
-      {
-        element: <GlobalLayout />,
-        id: "globalLayout",
-        errorElement: <ErrorBoundary />,
-        children: [
-          {
-            id: "events",
-            element: <EventsPage />,
-            index: true,
-            loader: eventsLoader,
-          },
-          {
-            path: "/event/:eventId",
-            id: "event",
-            element: <EventPage />,
-            loader: eventLoader,
-            action: eventActions,
-          },
-          {
-            path: "/event/new",
-            id: "newEvent",
-            element: <NewEventPage />,
-            action: createEvent,
-          },
-        ],
-      },
-    ],
+    future: {
+      // v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      // v7_startTransition: true,
+    },
   },
-]);
+);
 
 // @ts-ignore
 ReactDOM.createRoot(document.getElementById("root")).render(
