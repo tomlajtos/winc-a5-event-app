7c83fe499ec37e9909dec81eda30be3092c2d22e
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-15 21:19:38 +0100

    refactor: remove validation and toast call on click

--- src/components/forms/buttons/SaveEditButton.jsx
+++ src/components/forms/buttons/SaveEditButton.jsx
@@ -1,9 +1,9 @@
-import { useToast, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
-export const SaveEditButton = ({ errors }, props) => {
-  const toast = useToast();
+export const SaveEditButton = (props) => {
   const variant = props.variant ? props.variant : "base";
   const size = props.size ? props.size : "lg";
+
   return (
     <Button
       type="submit"
@@ -11,19 +11,6 @@ export const SaveEditButton = ({ errors }, props) => {
       name="intent"
       value="edit"
       size={size}
-      onClick={(e) => {
-        if (errors.size > 0) {
-          e.preventDefault();
-          toast({
-            title: "Editing is incomplete",
-            description: "Please complete the required fields.",
-            duration: 4000,
-            position: "top",
-            status: "error",
-            isClosable: true,
-          });
-        }
-      }}
       {...props}
     >
       Save
--- src/components/forms/buttons/SaveNewButton.jsx
+++ src/components/forms/buttons/SaveNewButton.jsx
@@ -1,32 +1,19 @@
-import { useToast, Button } from "@chakra-ui/react";
-import { validateAll } from "../../../io/validate";
+import { Button } from "@chakra-ui/react";
 
-export const SaveNewButton = ({ categoryIds, setErrors }, props) => {
-  const toast = useToast();
+export const SaveNewButton = (props) => {
   const variant = props.variant ? props.variant : "base";
   const size = props.size ? props.size : "lg";
   return (
     <Button
       type="submit"
       form="new-event-form"
+      name="intent"
+      value="addNew"
       variant={variant}
       size={size}
       colorScheme="purple"
-      onClick={(e) => {
-        // get and use errors for validation that are not yet set as state
-        const validity = validateAll(categoryIds, setErrors);
-
-        if (validity.isInvalid) {
-          e.preventDefault();
-          toast({
-            title: "Event information is incomplete",
-            description: "Please complete the required fields.",
-            duration: 4000,
-            position: "top",
-            status: "error",
-            isClosable: true,
-          });
-        }
+      onClick={() => {
+        console.log("saving new event");
       }}
       {...props}
     >
