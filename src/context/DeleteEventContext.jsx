edacf4ac8118d289b447faf9449b87bb7b76be54
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-16 14:16:07 +0100

    Add DeleteEventContext to be used in EventPage

---
+++ src/context/DeleteEventContext.jsx
@@ -0,0 +1,18 @@
+import React, { useContext, createContext } from "react";
+export const DeleteEventContext = createContext(null);
+
+export const DeleteEventContextProvider = ({ value, children }) => {
+  return (
+    <DeleteEventContext.Provider value={value}>
+      {children}
+    </DeleteEventContext.Provider>
+  );
+};
+
+export const useDeleteEvent = () => {
+  const context = useContext(DeleteEventContext);
+  if (!context) {
+    throw new Error("useDeleteEvent must be used within DeleteEventContext");
+  }
+  return context;
+};
