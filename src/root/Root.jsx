bd8a2f3bebefbee91ae3b768332b1da60d32410c
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-19 21:45:21 +0100

    refactor: remove loader > fetch same data in App

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,60 +1,40 @@
 // React and RRouter imports
-import { useRouteLoaderData, useLoaderData, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+// Context imports
 // Chakra-ui imports
 import { Box } from "@chakra-ui/react";
-// Context imports
-import { FilterContextProvider } from "../context/FilterContext.jsx";
-import { SearchContextProvider } from "../context/SearchContext.jsx";
 // Component imports
 import { AppHeader } from "./app-header/AppHeader";
-// utils imports
-import { fetchData } from "../io/fetch.js";
-import { Logger } from "../util/Logger.jsx";
-import { createCategoryIdsArr } from "../io/inputUtils.js";
-
-export const loader = async () => {
-  const categories = await fetchData("/categories");
-  const users = await fetchData("/users");
-  // TODO: check if this does make loading much longer
-  const categoryIds = createCategoryIdsArr(categories);
-  return { categories, categoryIds, users };
-};
+// utils and i/o imports
+import { Logger } from "../util/Logger";
 
 export const Root = () => {
-  const { categories, categoryIds, users } = useLoaderData();
-  const isMinimumLoaded = categories.length > 0 && categoryIds.length > 0;
-  if (!isMinimumLoaded) {
-    return null;
-  }
-
   return (
-    <Box
-      className="root-container"
-      width="100%"
-      minHeight="100vh"
-      display="flex"
-      flexDirection="column"
-      mx={0}
-      px={0}
-      b={8}
-      background="gray.200"
-    >
-      <Logger type="render" target="component" name="root" level={0} />
-      <SearchContextProvider>
-        <FilterContextProvider categoryIds={categoryIds}>
-          <AppHeader />
-          <Box
-            className="root-outlet container"
-            display="flex"
-            pt={0}
-            backgroundColor="transparent"
-            height="full"
-            flex="1"
-          >
-            <Outlet />
-          </Box>
-        </FilterContextProvider>
-      </SearchContextProvider>
-    </Box>
+    <Logger name="root" type="root">
+      <Box
+        className="root-container"
+        width="100%"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        mx={0}
+        px={0}
+        background="gray.200"
+      >
+        <AppHeader />
+
+        <Box
+          className="root-outlet container"
+          display="flex"
+          pt={0}
+          backgroundColor="transparent"
+          height={`${window.innerHeight - 95}px`}
+          flex="1"
+          overflowY="hidden"
+        >
+          <Outlet />
+        </Box>
+      </Box>
+    </Logger>
   );
 };
