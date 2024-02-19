7b043899cd8e48b6d036430376964e15441c7e7d
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-19 21:35:52 +0100

    Add separate Router component

---
+++ src/Router.jsx
@@ -0,0 +1,62 @@
+// React Router imports
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// App component imports
+import { Root } from "./root/Root";
+import { ErrorBoundary } from "./error-boundaries/ErrorBoundary";
+import {
+  EventsPage,
+  loader as eventsLoader,
+} from "./pages/events-page/EventsPage";
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      id: "root",
+      element: <Root />,
+      errorElement: <ErrorBoundary />,
+      children: [
+        {
+          path: "/",
+          id: "events",
+          loader: eventsLoader,
+          element: <EventsPage />,
+        },
+        {
+          path: "event/:eventId",
+          id: "event",
+          async lazy() {
+            let { EventPage, loader } = await import("./pages/EventPage");
+            let { action } = await import("./io/mutate");
+            return {
+              loader: loader,
+              action: action,
+              element: <EventPage />,
+            };
+          },
+        },
+        {
+          path: "event/new",
+          id: "newEvent",
+          async lazy() {
+            let { NewEventPage } = await import("./pages/NewEventPage");
+            let { action } = await import("./io/add");
+            return {
+              action: action,
+              element: <NewEventPage />,
+            };
+          },
+        },
+      ],
+    },
+  ],
+  {
+    future: {
+      v7_normalizeFormMethod: true,
+    },
+  },
+);
+
+export const Router = ({ fallbackElement }) => {
+  return <RouterProvider router={router} fallbackElement={fallbackElement} />;
+};
