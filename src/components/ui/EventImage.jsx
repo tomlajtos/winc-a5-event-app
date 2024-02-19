b8d1bba65d932c630918a8b016bf305df0d27274
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-19 22:28:25 +0100

    refactor: make EventImage a global ui component

--- src/components/ui/EventImage.jsx
+++ src/components/ui/EventImage.jsx
@@ -1,30 +1,31 @@
-import { Box, Center, Image, Text } from "@chakra-ui/react";
-import { Logger } from "../../util/Logger";
+import { useMemo } from "react";
+import { Box, Image } from "@chakra-ui/react";
+import { ImagePlaceholder } from "./ImagePlaceholder";
 
-export const EventImage = ({ event }) => {
-  return (
-    <Box>
-      <Logger type="render" target="component" name="EventImage" level={4} />
-      {event.image ? (
-        <Image
-          display="block"
-          src={event.image}
-          boxSize="300px"
-          objectFit="cover"
-          rounded="2xl"
-        />
-      ) : (
-        <Center
-          boxSize="300px"
-          rounded="2xl"
-          backgroundColor={"purple.800"}
-          color={"cyan.100"}
-        >
-          <Text fontSize={"2xl"} textAlign="center">
-            {event.title}
-          </Text>
-        </Center>
-      )}
-    </Box>
-  );
+export const EventImage = ({ event, ...props }) => {
+  const size = props.size ? props.size : "206px";
+  const rounded = props.rounded ? props.rounded : "";
+
+  const eventImage = useMemo(() => {
+    return (
+      <Box>
+        {event.image ? (
+          <Image
+            src={event.image}
+            boxSize={size}
+            objectFit="cover"
+            rounded={rounded}
+          />
+        ) : (
+          <ImagePlaceholder
+            boxSize={size}
+            rounded={rounded}
+            text={event.title}
+          />
+        )}
+      </Box>
+    );
+  }, [event.image]);
+
+  return eventImage;
 };
