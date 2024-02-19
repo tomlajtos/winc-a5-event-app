e742596c0dfe0f59128492cc7da7388557cb4149
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-19 22:16:46 +0100

    Tidy up ErrorBoundary

--- src/error-boundaries/ErrorBoundary.jsx
+++ src/error-boundaries/ErrorBoundary.jsx
@@ -1,22 +1,23 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError /*, isRouteErrorResponse*/ } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import { ErrorUi } from "./ErrorUi";
-import { pretifyError } from "../util/error";
+import { log } from "../util/log";
 
 export const ErrorBoundary = () => {
   const error = useRouteError();
-  const prettyError = pretifyError(error);
+  log.value("error @ ErrorBoundary", error);
   return (
     <Box
-      className="event-page-container"
+      className="error-boundary-container"
       width="100%"
       maxW="1280px"
       marginX="auto"
-      bg="gray.900"
+      bg="blackAlpha.700"
       pt="10%"
-      flexGrow="1"
+      color="white"
+      flex={"1"}
     >
-      <ErrorUi error={prettyError} />;
+      <ErrorUi err={error} />;
     </Box>
   );
 };
