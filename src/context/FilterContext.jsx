bbe66f8d8a85b66fb6bd5f47f7ba7f20753071b4
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-19 22:15:02 +0100

    refactor(optimize): rename state variables and memoize

--- src/context/FilterContext.jsx
+++ src/context/FilterContext.jsx
@@ -1,33 +1,48 @@
-import { createContext, useContext, useState } from "react";
-import { useOutletContext, useRouteLoaderData } from "react-router-dom";
+import { useState, useMemo, createContext, useContext } from "react";
+import { useStaticData } from "./StaticDataContext";
 import { Logger } from "../util/Logger";
+// import { log } from "../util/log";
 
 export const FilterContext = createContext({});
 FilterContext.displayName = "FilterContext";
 
-export const FilterContextProvider = ({ children /*categoryIds*/ }) => {
-  const { categoryIds } = useRouteLoaderData("root");
-  console.log("FILTER C categoryIds from OutletContext", categoryIds);
+export const FilterContextProvider = ({ children }) => {
+  const { categoryIds } = useStaticData();
+  const [categoryFilters, setCategoryFilters] = useState(categoryIds);
 
-  const [filters, setFilters] = useState([...categoryIds]);
+  // helper for category filtering
+  const hasFilterCategory = (catIds, catFilters) => {
+    const filters = new Set(catFilters);
+    return catIds.filter((id) => filters.has(id));
+  };
 
-  return filters ? (
-    <FilterContext.Provider value={{ filters, setFilters }}>
-      <Logger
-        type="render"
-        target="context"
-        name="Filter.Context.Provider"
-        level={2}
-      />
-      {children}
+  const filterEventsByCategories = (events) =>
+    events.filter(
+      (event) =>
+        hasFilterCategory(event.categoryIds, categoryFilters).length > 0,
+    );
+
+  const contextValue = useMemo(() => {
+    return {
+      categoryFilters,
+      setCategoryFilters,
+      filterEventsByCategories,
+    };
+  }, [categoryFilters, setCategoryFilters, filterEventsByCategories]);
+
+  return (
+    <FilterContext.Provider value={contextValue}>
+      <Logger type="context" name="filter-context" level={1}>
+        {children}
+      </Logger>
     </FilterContext.Provider>
-  ) : null;
+  );
 };
 
-export const useFilters = () => {
+export const useFilterContext = () => {
   const context = useContext(FilterContext);
   if (!context) {
-    throw new Error("useFilters must be used within FilterContext");
+    throw new Error("useFilterContext must be used within FilterContext");
   }
   return context;
 };
--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -1,30 +1,39 @@
-import { createContext, useContext, useState } from "react";
-import { Logger } from "../util/Logger";
+import { useState, useMemo } from "react";
+import { createContext, useContext } from "react";
+import { Logger } from "./../util/Logger";
 
-export const SearchContext = createContext({});
+export const SearchContext = createContext(null);
 SearchContext.displayName = "SearchContext";
 
 export const SearchContextProvider = ({ children }) => {
-  const [searchQ, setSearchQ] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+
+  const filterEventsBySearchValue = (events) =>
+    events.filter((event) =>
+      event.title.toLowerCase().includes(searchValue.toLowerCase()),
+    );
+
+  const contextValue = useMemo(() => {
+    return {
+      searchValue,
+      setSearchValue,
+      filterEventsBySearchValue,
+    };
+  }, [searchValue, setSearchValue, filterEventsBySearchValue]);
 
   return (
-    <SearchContext.Provider value={{ searchQ, setSearchQ }}>
-      <Logger
-        type="render"
-        target="context"
-        name="Search.Context.Provider"
-        level={2}
-      />
-      <Logger type="value" name="SearchContext > searchQ:" value={searchQ} />
-      {children}
-    </SearchContext.Provider>
+    <Logger type="context" name="search-context" level={2}>
+      <SearchContext.Provider value={contextValue}>
+        {children}
+      </SearchContext.Provider>
+    </Logger>
   );
 };
 
-export const useSearchQuery = () => {
+export const useSearchContext = () => {
   const context = useContext(SearchContext);
   if (!context) {
-    throw new Error("useSearchQuery must be used within SearchContext");
+    throw new Error("useSearchContext must be used within SearchContext");
   }
   return context;
 };
