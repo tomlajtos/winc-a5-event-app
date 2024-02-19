bf5eca0273f6d981da4cc270b2888a289e1ea28d
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-19 22:31:57 +0100

    refactor: add better error handling

--- src/io/mutate.js
+++ src/io/mutate.js
@@ -1,17 +1,52 @@
 import { redirect } from "react-router-dom";
-export const action = async ({ request, params }) => {
-  const formData = await request.formData();
-  const intent = formData.get("intent");
+import { validateFormDataInAction, generateHttpError } from "./validate";
+import { log } from "../util/log";
 
-  if (intent === "edit") {
-    const formDataObject = Object.fromEntries(await formData);
+export const action = async ({ request, params, signal }) => {
+  const formData = await request.formData();
+  const formDataObject = Object.fromEntries(await formData);
+  const intent = formData.get("intent");
+  const method = request.method;
+
+  // obj to return as result on edit success
+  const result = {
+    requestMethod: method,
+    success: false,
+  };
+
+  // base obj to return on error with edit/delete
+  const error = {
+    id: params.eventId,
+    requestMethod: method,
+    name: `Unsuccessful Action: ${intent} event`,
+    error: {},
+    data: {},
+  };
+
+  /* UPDATE event */
+  if (request.method === "PATCH" && intent === "edit") {
+    validateFormDataInAction(formDataObject, error);
+
+    // validate form data before fetch
+    if (Object.keys(error.error).length > 0) {
+      log.error("Input Error @mutate>action", error);
+      return error;
+    }
+
+    // convert categoryIds from strings to numbers - to addhere to original events.json formatting
     formDataObject.categoryIds = formDataObject.categoryIds
-      .split(",")
-      .map((id) => Number(id));
+      .split("")
+      .reduce((resArr, char) => {
+        if (char !== ",") {
+          resArr = [...resArr, Number(char)];
+        }
+        return resArr;
+      }, []);
 
     const response = await fetch(
       `http://localhost:3003/events/${params.eventId}`,
       {
+        signal,
         method: "PATCH",
         body: JSON.stringify(formDataObject),
         headers: {
@@ -20,18 +55,28 @@ export const action = async ({ request, params }) => {
       },
     );
 
-    return { formData: formDataObject, response };
+    if (!response.ok) {
+      log.error("HTTP error @mutate>UPDATE:", response, "\n", request);
+      return generateHttpError(error, response, formDataObject);
+    }
+
+    result.success = response.ok;
+    result.formData = formDataObject;
+    return result;
   }
 
-  if (intent === "delete") {
-    const response = fetch(`http://localhost:3003/events/${params.eventId}`, {
-      method: "DELETE",
-    });
+  /* DELETE event */
+  if (request.method === "DELETE") {
+    const response = await fetch(
+      `http://localhost:3003/events/${params.eventId}`,
+      {
+        method: "DELETE",
+      },
+    );
     if (!response.ok) {
-      return response;
+      log.error("HTTP error @mutate>DELETE:", response, "\n", request);
+      return generateHttpError(error, response, formDataObject);
     }
     return redirect("/");
   }
-
-  throw new Error(`The form was submitted with an invalid intent: ${intent}!`);
 };
