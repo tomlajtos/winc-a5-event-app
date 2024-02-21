61920c4f87feb0cdcbc576a00dd34159eb3fc858
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-21 22:34:22 +0100

    refactor: use custom hook `useMyAsyncError` to catch HTTP errors
    'My' in hook name is to avoid confusion /w React Router's useAsyncError

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import { getData } from "../io/fetch";
+import { useMyAsyncError } from "./useMyAsyncError";
 
 export const useData = (url) => {
+  const throwAsyncError = useMyAsyncError();
   const [data, setData] = useState([]);
   useEffect(() => {
-    getData(url, setData);
+    getData(url, setData, throwAsyncError);
   }, []);
-
   return data;
 };
