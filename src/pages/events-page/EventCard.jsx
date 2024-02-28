3c65bc29ce7d1216bf88e8d65f3f003aa935581b
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-28 16:20:07 +0100

    refactor: memoize component content for better performance

--- src/pages/events-page/EventCard.jsx
+++ src/pages/events-page/EventCard.jsx
@@ -2,28 +2,18 @@
 
 // React and React Router imports
 import { useMemo } from "react";
-import { useRouteLoaderData } from "react-router-dom";
 // chakra-ui imports
 import { Heading } from "@chakra-ui/react";
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Center,
-  Image,
-  Stack,
-  Text,
-  Tag,
-} from "@chakra-ui/react";
+import { Card, CardBody, CardFooter, Stack, Text, Tag } from "@chakra-ui/react";
+// context
+import { useStaticData } from "../../context/StaticDataContext.jsx";
+//components
+import { EventImage } from "../../components/ui/EventImage.jsx";
 // Util and i/o imports
 import { formatDateAndTime } from "../../util/datetime.js";
-import { Logger } from "../../util/Logger";
-import { log } from "../../util/log.js";
-// Asset imports
-import placeholderImgUrl from "../../assets/eventImgPlaceholder_300.svg";
 
 export const EventCard = ({ event }) => {
-  const { categories } = useRouteLoaderData("root");
+  const { categories } = useStaticData();
 
   const eventCategories = categories
     .filter((category) => event.categoryIds.includes(category.id))
@@ -35,69 +25,61 @@ export const EventCard = ({ event }) => {
   );
   const end = useMemo(() => formatDateAndTime(event.endTime), [event.endTime]);
 
-  const image = event.image === "" ? placeholderImgUrl : event.image;
+  const card = useMemo(() => {
+    return (
+      <Card
+        background="gray.50"
+        width="lg"
+        height="240px"
+        direction="row"
+        variant="outline"
+        py={4}
+        px={3}
+      >
+        <EventImage event={event} size="206px" />
+        <Stack flex={1}>
+          <CardBody pt={0} pb={2} pr={0}>
+            <Heading size="lg" noOfLines={1}>
+              {event.title}
+            </Heading>
+            <Stack pt={0} spacing={1} w="full">
+              {start.date === end.date ? (
+                <Text py={1} maxH={14} fontSize="lg">
+                  {start.date}
+                  {", "}
+                  {start.time}
+                  {" - "}
+                  {end.time}
+                </Text>
+              ) : (
+                <Text py={1} maxH={14} fontSize="lg">
+                  {start.date}
+                  {", "}
+                  {start.time}
+                  {" - "}
+                  {end.date}
+                  {", "}
+                  {end.time}
+                </Text>
+              )}
+              <Text pt={4} height={16} noOfLines={2} color="gray.700">
+                {event.description}
+              </Text>
+            </Stack>
+          </CardBody>
+          <CardFooter align="start" pt={0} pb={2}>
+            <Stack direction="row" spacing={2} pt={1}>
+              {eventCategories.map((category) => (
+                <Tag key={category} colorScheme="purple">
+                  {category}
+                </Tag>
+              ))}
+            </Stack>
+          </CardFooter>
+        </Stack>
+      </Card>
+    );
+  }, [event]);
 
-  return (
-    <Card
-      background="gray.50"
-      width="lg"
-      height="240px"
-      direction="row"
-      variant="outline"
-      py={4}
-      px={3}
-    >
-      {image ? (
-        <Image
-          boxSize="206px"
-          objectFit="cover"
-          src={event.image === "" ? placeholderImgUrl : event.image}
-        />
-      ) : (
-        <Center boxSize="206px" bg="gray.300">
-          Loading...
-        </Center>
-      )}
-      <Stack flex={1}>
-        <CardBody pt={0} pb={2} pr={0}>
-          <Heading size="lg" noOfLines={1}>
-            {event.title}
-          </Heading>
-          <Stack pt={0} spacing={1} w="full">
-            {start.date === end.date ? (
-              <Text py={1} maxH={14} fontSize="lg">
-                {start.date}
-                {", "}
-                {start.time}
-                {" - "}
-                {end.time}
-              </Text>
-            ) : (
-              <Text py={1} maxH={14} fontSize="lg">
-                {start.date}
-                {", "}
-                {start.time}
-                {" - "}
-                {end.date}
-                {", "}
-                {end.time}
-              </Text>
-            )}
-            <Text pt={4} height={16} noOfLines={2} color="gray.700">
-              {event.description}
-            </Text>
-          </Stack>
-        </CardBody>
-        <CardFooter align="start" pt={0} pb={2}>
-          <Stack direction="row" spacing={2} pt={1}>
-            {eventCategories.map((category) => (
-              <Tag key={category} colorScheme="purple">
-                {category}
-              </Tag>
-            ))}
-          </Stack>
-        </CardFooter>
-      </Stack>
-    </Card>
-  );
+  return card;
 };
