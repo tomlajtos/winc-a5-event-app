f563e877a380eaeb9afa6d8eb4eb285a7cc343d2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-28 16:15:04 +0100

    refactor: pass layout as a prop instead using ResizeContext(removed)

--- src/root/app-header/header-menu/dropdown-menu/DropdownMenu.jsx
+++ src/root/app-header/header-menu/dropdown-menu/DropdownMenu.jsx
@@ -7,57 +7,55 @@ import {
   MenuDivider,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { useResizeData } from "../../../../context/ResizeContext";
 import { Search } from "../search/Search";
 import { Navigation } from "./Navigation";
 import { CategoryFilters } from "../CategoryFilters";
 import { Logger } from "../../../../util/Logger";
 
-export const DropdownMenu = () => {
-  const { menuLayout } = useResizeData();
-
+export const DropdownMenu = ({ layout }) => {
   return (
-    <Menu
-      closeOnSelect={false}
-      gutter={12}
-      isLazy={true}
-      lazyBehavior="keepMounted"
-    >
-      <Logger type="render" target="component" name="dropdown-menu" level={3} />
-      <MenuButton
-        as={IconButton}
-        arai-label="Options"
-        icon={<HamburgerIcon />}
-        variant="solid"
-      />
-      <MenuList
-        py={4}
-        direction={["column", "row"]}
-        minWidth={"350px"}
-        alignItems="center"
-        spacing={0}
-        backgroundColor="whiteAlpha.900"
-        color="gray.900"
-        border="none"
+    <Logger name="DropdownMenu" level={3}>
+      <Menu
+        closeOnSelect={false}
+        gutter={12}
+        isLazy={true}
+        lazyBehavior="keepMounted"
       >
-        {menuLayout == "min" && (
-          <Search
-            props={{
-              width: "90%",
-              mx: "auto",
-              pb: 3,
-            }}
-            inputProps={{
-              borderColor: "gray.600",
-              backgroundColor: "white",
-              textColor: "gray.900",
-            }}
-          />
-        )}
-        <Navigation />
-        <MenuDivider borderColor="gray.400" width="95%" mx="auto" />
-        <CategoryFilters />
-      </MenuList>
-    </Menu>
+        <MenuButton
+          as={IconButton}
+          arai-label="Options"
+          icon={<HamburgerIcon />}
+          variant="solid"
+        />
+        <MenuList
+          py={4}
+          direction={["column", "row"]}
+          minWidth={"350px"}
+          alignItems="center"
+          spacing={0}
+          backgroundColor="whiteAlpha.900"
+          color="gray.900"
+          border="none"
+        >
+          {layout == "min" && (
+            <Search
+              props={{
+                width: "90%",
+                mx: "auto",
+                pb: 3,
+              }}
+              inputProps={{
+                borderColor: "gray.600",
+                backgroundColor: "white",
+                textColor: "gray.900",
+              }}
+            />
+          )}
+          <Navigation />
+          <MenuDivider borderColor="gray.400" width="95%" mx="auto" />
+          <CategoryFilters />
+        </MenuList>
+      </Menu>
+    </Logger>
   );
 };
