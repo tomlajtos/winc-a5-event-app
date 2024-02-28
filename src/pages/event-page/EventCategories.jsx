43f6cd6a70c67e1d2347048ede0df30ac07b6225
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-28 16:28:33 +0100

    refactor: use new useStaticData hook instead of useRouteLoaderData

--- src/pages/event-page/EventCategories.jsx
+++ src/pages/event-page/EventCategories.jsx
@@ -1,24 +1,21 @@
 import { Stack, Tag } from "@chakra-ui/react";
-import { useRouteLoaderData } from "react-router-dom";
+import { useStaticData } from "../../context/StaticDataContext";
 import { Logger } from "../../util/Logger";
 
 export const EventCategories = ({ event }) => {
-  const { categories } = useRouteLoaderData("root");
+  const { categories } = useStaticData();
+
   return (
-    <Stack direction={"row"} spacing={2} pt={1}>
-      <Logger
-        type="render"
-        target="component"
-        name="Event Categories"
-        level={4}
-      />
-      {categories.map((category) =>
-        event.categoryIds.includes(category.id) ? (
-          <Tag key={category.name} size="lg" colorScheme={"purple"}>
-            {category.name}
-          </Tag>
-        ) : null,
-      )}
-    </Stack>
+    <Logger name="EventCategories" level={5}>
+      <Stack direction={"row"} spacing={2} pt={1}>
+        {categories.map((category) =>
+          event.categoryIds.includes(category.id) ? (
+            <Tag key={category.name} size="lg" colorScheme={"purple"}>
+              {category.name}
+            </Tag>
+          ) : null,
+        )}
+      </Stack>
+    </Logger>
   );
 };
