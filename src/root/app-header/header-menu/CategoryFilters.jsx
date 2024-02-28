a4f969a3b3444e32ec1d9d2ec631b73db14f7a9d
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-28 16:47:10 +0100

    refactor: move onChange handler to inputUtils, use new contexts

--- src/root/app-header/header-menu/CategoryFilters.jsx
+++ src/root/app-header/header-menu/CategoryFilters.jsx
@@ -1,55 +1,46 @@
 import React from "react";
-import { useRouteLoaderData } from "react-router-dom";
 import { MenuItemOption, MenuOptionGroup } from "@chakra-ui/react";
-import { useFilters } from "../../../context/FilterContext";
+import { useStaticData } from "../../../context/StaticDataContext";
+import { useFilterContext } from "../../../context/FilterContext";
+import { handleFilterChange } from "../../../io/inputUtils";
 import { Logger } from "../../../util/Logger";
 
 export const CategoryFilters = () => {
-  const { filters, setFilters } = useFilters();
-  const { categories } = useRouteLoaderData("root");
-
-  const handleFilterChange = (filters, inputValue, setFn) => {
-    let newFilters = [...filters];
-    if (!newFilters.includes(inputValue)) {
-      newFilters = Array.from(new Set([Number(inputValue), ...newFilters]));
-    } else {
-      newFilters = newFilters.filter((q) => q !== Number(inputValue));
-    }
-    setFn(newFilters);
-  };
+  const { categories } = useStaticData();
+  const { categoryFilters, setCategoryFilters } = useFilterContext();
 
   return (
-    <MenuOptionGroup
-      px={2}
-      title="Categories"
-      type="checkbox"
-      name="categoryIds"
-      defaultValue={filters.map((id) => id.toString())}
-      fontSize="xl"
-      fontWeight="thin"
-    >
-      <Logger
-        type="render"
-        target="component"
-        name="CategoryFilters"
-        level={4}
-      />
-      {categories.map((category) => (
-        <MenuItemOption
-          key={category.id}
-          id={`${category.id}`}
-          name="categoryIds"
-          value={`${category.id}`}
-          fontSize="lg"
-          backgroundColor="transparent"
-          pl={6}
-          onClick={() => {
-            handleFilterChange(filters, category.id, setFilters);
-          }}
-        >
-          {category.name}
-        </MenuItemOption>
-      ))}
-    </MenuOptionGroup>
+    <Logger name="CategoryFilters" level={4}>
+      <MenuOptionGroup
+        px={2}
+        title="Categories"
+        type="checkbox"
+        name="categoryIds"
+        defaultValue={categoryFilters.map((id) => id.toString())}
+        fontSize="xl"
+        fontWeight="thin"
+      >
+        {categories.map((category) => (
+          <MenuItemOption
+            key={category.id}
+            id={`${category.id}`}
+            name="categoryIds"
+            value={`${category.id}`}
+            fontSize="lg"
+            backgroundColor="transparent"
+            pl={6}
+            onClick={() => {
+              handleFilterChange(
+                categoryFilters,
+                category.id,
+                setCategoryFilters,
+              );
+            }}
+          >
+            {category.name}
+          </MenuItemOption>
+        ))}
+      </MenuOptionGroup>
+    </Logger>
   );
 };
