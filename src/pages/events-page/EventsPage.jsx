ae05a73d4aae86dea25e817725c7c56bcb64ea32
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-28 16:27:00 +0100

    refactor: update fetchData argument and set sticky top to 0

--- src/pages/events-page/EventsPage.jsx
+++ src/pages/events-page/EventsPage.jsx
@@ -1,7 +1,7 @@
 // React imports
 import React from "react";
-// React Router imports
 import { useLoaderData } from "react-router-dom";
+// React Router imports
 // ChakraUi imports
 import { Box } from "@chakra-ui/react";
 // Context and custom hook imports
@@ -9,26 +9,28 @@ import { Box } from "@chakra-ui/react";
 import { PageTitle } from "../PageTitle";
 import { EventsList } from "./EventsList";
 // Util imports
-import { fetchData } from "../../io/fetch.js";
+import { fetchData } from "../../io/fetch";
 import { Logger } from "../../util/Logger";
 
-export const loader = async () => fetchData("/events");
+export const loader = async () => fetchData("events");
 
 export const EventsPage = () => {
   const events = useLoaderData();
 
   return (
-    <Box
-      className="events-page-container"
-      width="100%"
-      maxW="1280px"
-      flexGrow="1"
-      marginX="auto"
-      bg="gray.100"
-    >
-      <Logger type="render" target="page" name="EventsPage" level={0} />
-      <PageTitle title="Events" position="sticky" top="95px" />
-      <EventsList events={events} />
-    </Box>
+    <Logger type="page" name="EventsPage" level={4}>
+      <Box
+        className="events-page-container"
+        width="100%"
+        maxW="1280px"
+        flexGrow="1"
+        marginX="auto"
+        bg="gray.100"
+        overflowY="auto"
+      >
+        <PageTitle title="Events" position="sticky" top="0px" />
+        <EventsList events={events} />
+      </Box>
+    </Logger>
   );
 };
