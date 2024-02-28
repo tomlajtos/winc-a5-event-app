88f0a791e939db920e9646e558403a7fdf98bff3
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-02-28 16:46:07 +0100

    refactor: use new SearchContext update event handlers

--- src/root/app-header/header-menu/search/Search.jsx
+++ src/root/app-header/header-menu/search/Search.jsx
@@ -1,62 +1,77 @@
 import { useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { useDisclosure, Center, Input as CInput } from "@chakra-ui/react";
-
 import { PopupSearch } from "./PopupSearch";
-
+import { useSearchContext } from "../../../../context/SearchContext";
 import { Logger } from "../../../../util/Logger";
-import { useSearchQuery } from "../../../../context/SearchContext";
 
 export const Search = ({ inputProps, props }) => {
-  const { searchQ, setSearchQ } = useSearchQuery();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { searchValue, setSearchValue } = useSearchContext();
+
   const { pathname } = useLocation();
+  const popupSearchModal = useDisclosure();
   const popupSearch = useRef("");
 
+  // handle popupSearchModal closing
   const closePopupSearch = () => {
-    setSearchQ("");
-    onClose();
+    searchValue !== "" ? setSearchValue("") : searchValue;
+    popupSearchModal.onClose();
+  };
+
+  // close popupSearchModal if route location is "/"
+  if (pathname === "/" && popupSearchModal.isOpen) {
+    closePopupSearch();
+    searchValue !== "" ? setSearchValue("") : searchValue;
+  }
+
+  // event handlers for search input
+  const handleChange = () => (e) => setSearchValue(e.target.value);
+  const handleClick = () => {
+    if (pathname !== "/") {
+      searchValue !== "" ? setSearchValue("") : searchValue;
+      popupSearchModal.onOpen();
+    }
+  };
+
+  // open/close popupSearchModal on search input keydown
+  const handleKeyDown = (e) => {
+    if (pathname !== "/") {
+      e.key === "Enter"
+        ? popupSearchModal.onOpen()
+        : e.key === "Escape"
+          ? closePopupSearch()
+          : e.preventDefault();
+    }
   };
 
   return (
-    <Center {...props}>
-      <Logger type="render" target="component" name="Search" level={3} />
-      <CInput
-        type="search"
-        name="search"
-        variant={"outline"}
-        placeholder="Search for an event..."
-        rounded={"full"}
-        minW="225px"
-        maxW="500px"
-        px={6}
-        color="gray.200"
-        focusBorderColor="purple.300"
-        defaultValue={searchQ}
-        onChange={(e) => {
-          if (pathname == "/") {
-            setSearchQ(e.target.value);
-          }
-        }}
-        {...inputProps}
-        onClick={() => (pathname !== "/" ? onOpen() : closePopupSearch())}
-        onKeyDown={(e) =>
-          (() => {
-            if (pathname !== "/" && e.key === "Enter") {
-              onOpen();
-            } else if (pathname !== "/" && e.key === "Escape") {
-              closePopupSearch();
-            }
-          })()
-        }
-      />
-      {pathname !== "/" && (
-        <PopupSearch
-          inputRef={popupSearch}
-          isOpen={isOpen}
-          onClose={closePopupSearch}
+    <Logger name="search" level={7}>
+      <Center {...props}>
+        <CInput
+          type="search"
+          name="search"
+          variant={"outline"}
+          placeholder="Search for an event..."
+          rounded={"full"}
+          minW="225px"
+          maxW="500px"
+          px={6}
+          color="gray.200"
+          focusBorderColor="purple.300"
+          defaultValue={searchValue}
+          onChange={handleChange}
+          onClick={handleClick}
+          onKeyDown={handleKeyDown}
+          {...inputProps}
         />
-      )}
-    </Center>
+        {pathname !== "/" && (
+          <PopupSearch
+            inputRef={popupSearch}
+            isOpen={popupSearchModal.isOpen}
+            onClose={closePopupSearch}
+          />
+        )}
+      </Center>
+    </Logger>
   );
 };
