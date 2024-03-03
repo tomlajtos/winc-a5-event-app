6b610cd7f830e1376df9c21b9f4210e32a5a3251
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-03-03 23:09:05 +0100

    Add conditional rendering for error stack and responsive styles

--- src/error-boundaries/ErrorUi.jsx
+++ src/error-boundaries/ErrorUi.jsx
@@ -12,6 +12,7 @@ import {
   Spacer,
   Stack,
   Text,
+  Wrap,
 } from "@chakra-ui/react";
 import { prettifyError } from "../util/error";
 
@@ -21,7 +22,7 @@ export const ErrorUi = ({ error }) => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Container maxW="3xl">
+    <Container maxW={["full", null, "3xl"]}>
       <Alert
         status="error"
         px={[2, 4, 8]}
@@ -29,46 +30,65 @@ export const ErrorUi = ({ error }) => {
         display="flex"
         flexDirection="column"
         color="gray.50"
-        bg="blackAlpha.700"
+        bg="blackAlpha.500"
         rounded="2xl"
       >
         <AlertIcon boxSize="69px" pb={6} />
 
         <AlertTitle fontSize="lg">Something went wrong...</AlertTitle>
 
-        <AlertDescription>
-          <Flex direction="column" align="start" maxW="full">
+        <AlertDescription width="full">
+          <Stack direction="column" align="start" maxW="full">
             <Button
               onClick={onToggle}
               width="fit-content"
               size="sm"
               variant="unstyled"
-              textColor="gray.400"
+              textColor="gray.300"
               mt={4}
               alignSelf="center"
             >
               Toggle details
             </Button>
-            <Spacer height={2} />
-            <Collapse in={isOpen} animateOpacity>
-              <Stack py={2} spacing={2}>
-                <Text textAlign="left" fontWeight={600} fontSize="lg">
-                  {message}
-                </Text>
-                <Text textAlign="left" fontSize="lg" textDecor="underline">
-                  {name}:
-                </Text>
-                <Text textAlign="left" pl={2}>
-                  <Text as="span" mr={3} fontWeight="bolder">
-                    {status && status}
+            <Collapse
+              w={"250px"}
+              in={isOpen}
+              p={0}
+              m={0}
+              animateOpacity
+              bg="purple.400"
+            >
+              {!status && (
+                <Stack py={2} spacing={3}>
+                  <Text textAlign="left" fontSize="lg" textDecor="underline">
+                    {name}:
                   </Text>
-                  <Text as="span" mr={2}>
-                    {statusText && statusText}
+                  <Text textAlign="left" fontWeight={600} fontSize="lg">
+                    {message}
                   </Text>
-                  <Text as="span">{url && `(${url})`}</Text>
-                </Text>
-              </Stack>
-              <Box bg="gray.900" opacity="70%" p={4}>
+                </Stack>
+              )}
+              {status && (
+                <Stack py={2} spacing={2}>
+                  <Text textAlign="left" fontWeight={600} fontSize="lg">
+                    {message}
+                  </Text>
+                  <Text textAlign="left" fontSize="lg" textDecor="underline">
+                    {name}:
+                  </Text>
+                  <Text textAlign="left" pl={2}>
+                    <Text as="span" mr={3} fontWeight="bolder">
+                      {status}
+                    </Text>
+                    <Text as="span" mr={2}>
+                      {statusText && statusText}
+                    </Text>
+                    <Text as="span">{url && `(${url})`}</Text>
+                  </Text>
+                </Stack>
+              )}
+              <Spacer height={2} />
+              <Stack bg="blackAlpha.500" px={0} maxW="full">
                 {stackLines.map((line, index) =>
                   index !== 0 ? (
                     <Text
@@ -83,9 +103,9 @@ export const ErrorUi = ({ error }) => {
                     </Text>
                   ) : null,
                 )}
-              </Box>
+              </Stack>
             </Collapse>
-          </Flex>
+          </Stack>
         </AlertDescription>
       </Alert>
     </Container>
