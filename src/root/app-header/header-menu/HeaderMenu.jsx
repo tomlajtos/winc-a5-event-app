1d873978afcd84042f63c5f6cef97421a0a1cbde
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-03-06 13:13:47 +0100

    Update responsive styles

--- src/root/app-header/header-menu/HeaderMenu.jsx
+++ src/root/app-header/header-menu/HeaderMenu.jsx
@@ -1,18 +1,16 @@
-// React and RRouter imports
+// React and React Router imports
 import { useState, useEffect } from "react";
 // Chakra-ui imports
 import { Flex } from "@chakra-ui/react";
-// Context imports
 // Component imports
 import { DropdownMenu } from "./dropdown-menu/DropdownMenu";
 import { MenuBar } from "./menu-bar/MenuBar";
 import { Search } from "./search/Search";
-// Util import
 
 export const HeaderMenu = () => {
   const [layout, setLayout] = useState("");
 
-  // effect to track window resize for a menu layout dependent on screen width
+  // effect to track window resize for menu layout that is dependent on screen width
   useEffect(() => {
     const getLayout = () => {
       const minLayout = window.matchMedia("(max-width:768px)");
@@ -32,7 +30,16 @@ export const HeaderMenu = () => {
 
   return (
     <Flex align="center" gap={6}>
-      {layout !== "min" && <Search justifySelf="center" order={1} />}
+      {layout !== "min" && (
+        <Search
+          justifySelf="center"
+          order={1}
+          inputProps={{
+            _placeholder: { color: "gray.300" },
+            borderColor: "gray.200",
+          }}
+        />
+      )}
       {layout === "full" && <MenuBar />}
       {(layout === "min" || layout === "mid") && (
         <DropdownMenu layout={layout} />
