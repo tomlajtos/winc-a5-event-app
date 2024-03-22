0c75390b343d811013f05aa809392ebb6f7bf298
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-03-22 13:46:25 +0100

    fix: port in getData left on 3003 (for testing), revert to 3000

--- src/io/fetch.js
+++ src/io/fetch.js
@@ -25,7 +25,7 @@ export const handleHttpError = (response) => {
 // TODO: learn & add jsDOC comments
 export const getData = async (endpoint, setState, throwAsyncError) => {
   //example: (endpoint: "/categories", setState: setCategories)
-  const baseUrl = "http://127.0.0.1:3003";
+  const baseUrl = "http://127.0.0.1:3000";
   const url = `${baseUrl}/${endpoint}`;
   try {
     const response = await fetch(url);
